b8f9ee211b1852e6634d99f7c6abdde3e5cc2658
Colin McDaniel <colinmcdaniel@me.com>
2016-11-12 13:25:24 -0800

    Save and display results functionality

--- app/Components/Main.js
+++ app/Components/Main.js
@@ -16,7 +16,15 @@ var Main = React.createClass({
 	getInitialState: function(){
 		return {
 			searchTerm: "",
-			results: "",
+			searchStartYear: 0,
+			searchEndYear: 0,
+			results: [
+				{"title": "", "date": "", "url": ""},
+				{"title": "", "date": "", "url": ""},
+				{"title": "", "date": "", "url": ""},
+				{"title": "", "date": "", "url": ""},
+				{"title": "", "date": "", "url": ""}
+			],
 			history: [] /*Note how we added in this history state variable*/
 		}
 	},	
@@ -28,25 +36,40 @@ var Main = React.createClass({
 		})
 	},
 
+	setStartYear: function(startYear){
+		this.setState({
+			searchStartYear: startYear
+		});
+	},
+
+	setEndYear: function(endYear){
+		this.setState({
+			searchEndYear: endYear
+		});
+	},
+
 	// If the component changes (i.e. if a search is entered)... 
 	componentDidUpdate: function(prevProps, prevState){
 
-		if(prevState.searchTerm != this.state.searchTerm){
+		if(prevState.searchTerm != this.state.searchTerm || prevState.searchStartYear != this.state.searchStartYear || prevState.searchEndYear != this.state.searchEndYear){
 			console.log("UPDATED");
 
 			// Run the query for the address
-			helpers.runQuery(this.state.searchTerm)
+			helpers.runQuery(this.state.searchTerm, this.state.searchStartYear, this.state.searchEndYear)
 				.then(function(data){
 					if (data != this.state.results)
 					{
-						console.log("Address", data);
+						// console.log("Results", data);
 
 						this.setState({
 							results: data
 						})
 
+						// console.log("This should be an array!");
+						// console.log(data);
+
 						// After we've received the result... then post the search term to our history. 
-						helpers.postHistory(this.state.searchTerm)
+						helpers.postHistory(this.state.searchTerm, this.state.searchStartYear, this.state.searchEndYear)
 							.then(function(data){
 								console.log("Updated!");
 
@@ -96,19 +119,19 @@ var Main = React.createClass({
 				<div className="row">
 
 					<div className="jumbotron">
-						<h2 className="text-center">Address Finder!</h2>
+						<h2 className="text-center">New York Times Article Scrubber</h2>
 						<p className="text-center"><em>Enter a landmark to search for its exact address (ex: "Eiffel Tower").</em></p>
 					</div>
 
 					<div className="col-md-6">
 					
-						<Saved setTerm={this.setTerm}/>
+						<Search setTerm={this.setTerm} setStartYear={this.setStartYear} setEndYear={this.setEndYear}/>
 
 					</div>
 
 					<div className="col-md-6">
 				
-						<Results address={this.state.results} />
+						<Results results={this.state.results} />
 
 					</div>
 
@@ -116,7 +139,7 @@ var Main = React.createClass({
 
 				<div className="row">
 
-					<Search history={this.state.history}/> 
+					<Saved history={this.state.history}/> 
 
 				</div>
 
